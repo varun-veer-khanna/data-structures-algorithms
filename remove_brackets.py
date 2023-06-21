284bb06e7d6b6411c1406edbe1ec7cc0fd7af216
varun-veer-khanna <varun.veer.khanna@gmail.com>
2023-06-21 18:56:30 +0530

    Final Commit for the file

--- remove_brackets.py
+++ remove_brackets.py
@@ -6,39 +6,44 @@ Output the simplified string without parentheses.'''
 def remove_brackets(string):
     result_string=""
     sign_stack=[0] # 0 for positive and 1 for negative
-    brace_stack=[0] # 0 for opening and 1 for closing
+    
     if string:
+        #traversing the entire range of the string
         for i in range(0, len(string)):
-            if sign_stack[-1]==1 and brace_stack[-1]==0:
-                # means that there is an opening brace and a negative sign before it
-                # would require toggling of the signs which are to be subsequently processed
-                if string[i]=='-':
-                    result_string=result_string+"+"
-                    sign_stack.append(1)
-                elif string[i]=='(':
-                    brace_stack.append(0)
-                elif string[i]=='+':
-                    result_string=result_string+"-"
+            # braces and the sign before it play a role in deciding if the subsequent
+            # signs have to be toggled or not
+            if string[i]=='(':
+                if string[i-1]=='+':
+                    #setting the flag for a positive sign
                     sign_stack.append(0)
-                elif string[i]==')':
-                    brace_stack.append(1)
-                    sign_stack.pop()
-                else:
-                    result_string=result_string+string[i] 
+                elif string[i-1]=='-':
+                    #setting the flag for a negative sign
+                    sign_stack.append(1)
+            # the role of a previous opening brace ends with a closing brace
+            # hence, popping the previous sign flag before the opening brace
+            elif string[i]==')':
+                sign_stack.pop()
+            # for all other characters
             else:
-                if string[i]=='-':
-                    result_string=result_string+"-"
-                    sign_stack.append(0)
-                elif string[i]=='(':
-                    brace_stack.append(0)
-                elif string[i]=='+':
-                    result_string=result_string+"+"
-                    sign_stack.append(1)
-                elif string[i]==')':
-                    brace_stack.append(1)
-                    sign_stack.pop()
+                # if the sign is positive and the sign flag is negative
+                # toggling is required
+                if string[i]=='+' and sign_stack[-1]==1:
+                    result_string=result_string+'-'
+                    # alongwith toggling, the toggled sign must
+                    # be stored in the original string to guide 
+                    # oncoming opening braces
+                    string = string[:i] + '-' + string[i+1:]
+                    # as the sign in the original string is changed,
+                    # so must the sign flag
+                    sign_stack[-1]=0
+                elif string[i]=='-' and sign_stack[-1]==1:
+                    result_string=result_string+'+'
+                    string = string[:i] + '+' + string[i+1:]
+                    sign_stack[-1]=1
+                # if the character is anything else
                 else:
                     result_string=result_string+string[i]
+                
     return result_string
             
 
